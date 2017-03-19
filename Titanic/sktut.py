420b9832c719d8505fce4535d807653d2d040ddf
Ashu Goel <ashu020497@gmail.com>
2017-03-19 18:36:06 +0530

    Sklearn trial files

---
+++ Titanic/sktut.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import numpy as np
+X = pd.read_csv('titanic_data.csv')
+X = X.select_dtypes(include=[object])
+from sklearn.preprocessing import OneHotEncoder,LabelEncoder
+le = LabelEncoder()
+ohe = OneHotEncoder()
+'''for feature in X:
+	X[feature] = le.fit_transform(X[feature])
+	print X[feature]'''
+xt = ohe.fit_transform(X)
+print xt
